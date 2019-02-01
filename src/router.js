7e34a102bbfddaf97a59da588ebfe124bdd6216d
aamiaohui <907671273@qq.com>
2019-02-01 09:11:18 +0800

    后台管理系统页面组件

--- src/router.js
+++ src/router.js
@@ -3,37 +3,36 @@ import Router from 'vue-router'
 import Login from './views/Login.vue'
 import Main from './views/Main.vue'
 import NotFound from './views/NotFound.vue'
-import GlobalSettings from './views/GlobalSettings'
-import TableList from './views/TableList'
-import TableAdd from './views/TableAdd'
-import TableDelete from './views/TableDelete'
-import CategoryList from './views/CategoryList'
-import CategoryAdd from './views/CategoryAdd'
-import CategoryDelete from './views/CategoryDelete'
-import CategoryUpdate from './views/CategoryUpdate'
-import DishList from './views/DishList'
-import DishAdd from './views/DishAdd'
-import DishDelete from './views/DishDelete'
-import DishUpdate from './views/DishUpdate'
-import OrderList from './views/OrderList'
-import Security from './views/Security'
-import { Table } from 'element-ui';
+import GlobalSettings from './views/GlobalSettings.vue'
+import TableList from './views/TableList.vue'
+import TableAdd from './views/TableAdd.vue'
+import TableDelete from './views/TableDelete.vue'
+import CategoryList from './views/CategoryList.vue'
+import CategoryAdd from './views/CategoryAdd.vue'
+import CategoryDelete from './views/CategoryDelete.vue'
+import CategoryUpdate from './views/CategoryUpdate.vue'
+import DishList from './views/DishList.vue'
+import DishAdd from './views/DishAdd.vue'
+import DishDelete from './views/DishDelete.vue'
+import DishUpdate from './views/DishUpdate.vue'
+import OrderList from './views/OrderList.vue'
+import Security from './views/Security.vue'
 
 Vue.use(Router)
-
+//整个项目的路由词典：访问路径 <=> 视图组件
 export default new Router({
   routes: [
-    {path:'/',redirect:'login'},
-    {path:'/login',component:Login},
-    {
-      path:'/main',
-      component:Main,
-      children:[
-        {path:'',redirect:'/table/list'},
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { 
+      path: '/main', 
+      component: Main,
+      children: [
+        {path:'',redirect:'/table/list'},        
         {path:'/settings',component:GlobalSettings},
         {path:'/table/list',component:TableList},
         {path:'/table/add',component:TableAdd},
-        {path:'/table/delete',component:TableDelete}, 
+        {path:'/table/delete',component:TableDelete},
         {path:'/category/list',component:CategoryList},
         {path:'/category/add',component:CategoryAdd},
         {path:'/category/delete',component:CategoryDelete},
@@ -46,6 +45,6 @@ export default new Router({
         {path:'/security',component:Security},
       ]
     },
-    {path:'*',component:NotFound},
+    { path: '*', component: NotFound },
   ]
 })
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,63 +4,69 @@
       <div slot="header">管理员登录</div>
       <div>
         <el-form label-width="100px">
-          <el-form-item label="管理员名:">
+
+          <el-form-item label="管理员名：">
             <el-input v-model="formData.aname" placeholder="请输入管理员名"></el-input>
           </el-form-item>
-          <el-form-item label="登录密码:">
-            <el-input v-model="formData.apwd" placeholder="请输入登录密码" type="password"></el-input>
+        
+          <el-form-item label="登录密码：">
+            <el-input type="password" v-model="formData.apwd" placeholder="请输入管理员密码"></el-input>
           </el-form-item>
+
           <el-form-item>
             <el-button type="primary" @click="doLogin">登录</el-button>
             <el-button @click="doCancel">取消</el-button>
           </el-form-item>
+        
         </el-form>
       </div>
-  </el-card>
-  {{$store.state.globalSettings}}
+    </el-card>
   </div>
 </template>
+
 <script>
-export default {//普通组件的模型对象是函数
-   data(){
-     return {//表单中用户输入的两个数据
-       formData:{
-         aname:"admin",
-         apwd:"123456"
-       }
-     }
-   },
-   methods: {
-      doLogin(){//执行登录
-        var url = this.$store.state.globalSettings.apiUrl+`/admin/login/${this.formData.aname}/${this.formData.apwd}`;
-        console.log(url)
-        this.$axios.get(url).then((res)=>{
-          if(res.data.code==200){//登录成功
-          //把用户名存储到Vuex仓库
+export default {
+  data(){   //普通组件的模型数据是函数返回值
+    return {
+      formData: { //表单中用户输入的两个数据
+        aname: 'admin',
+        apwd: '123456'
+      }
+    }
+  },
+  methods:{
+    doLogin(){  //执行登录
+      var url = this.$store.state.globalSettings.apiUrl+`/admin/login/${this.formData.aname}/${this.formData.apwd}`;
+
+      this.$axios.get(url).then((res)=>{
+        if(res.data.code==200){ //登录成功
+          //把用户名存入Vuex存储仓库
           this.$store.commit('setAdminName',this.formData.aname);
           //执行视图跳转
-            this.$router.push('/main')
-          }else{//登录失败
-            this.$alert('用户名或密码有误！','登录失败',{type:'error'}.then(()=>{}))
-          }
-        }).catch((err)=>{
-          console.log(err)
-        })
-     },
-     doCancel(){
-       this.formData.aname="";
-       this.formData.apwd=""
-     }
-   }
-}
+          this.$router.push('/main');
+        }else { //登录失败
+          this.$alert('用户名或密码有误！','登录失败',{type:'error'}).then(()=>{}).catch(()=>{});
+        }
+      }).catch((err)=>{
+        console.log(err);
+      })
+    },
+    doCancel(){ //清除用户的输入
+      this.formData.aname = '';
+      this.formData.apwd = '';
+    }
+  }
+}  
 </script>
+
 <style lang="scss">
-  .xfn-login-card{
-    width:450px;
-    margin:150px auto;
-    .el-card__header{
-      text-align:center;
-      font-size:1.2em;
+  .xfn-login-card {
+    width: 450px;
+    margin: 100px auto;
+
+    .el-card__header {
+      text-align: center;
+      font-size: 1.2em;
     }
   }
 </style>
